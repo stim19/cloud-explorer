3b113b23bd67757da865762763aff4a7e4f72b0d
stim19 <yoshiyomimiyori@gmail.com>
2025-06-28 21:50:52 -0400

    updated project stuff, fixes

--- utils/auth.py
+++ utils/auth.py
@@ -10,24 +10,25 @@ class User(db.Model, UserMixin):
     password_hash=db.Column(db.String(128), nullable=False)
     role=db.Column(db.String(10), default='user', nullable=False)
 
-#Write only
-@property
-def password(self):
-    raise AttributeError("Password is write-only")
+    #Write only
+    @property
+    def password(self):
+        raise AttributeError("Password is write-only")
 
-@password.setter
-def password(self, password):
-    self.password_hash=generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.password_hash=generate_password_hash(password)
 
-def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    def check_password(self, password):
+            return check_password_hash(self.password_hash, password)
 
-def __repr__(self):
-    return f'<User {self.username}>'
+    def __repr__(self):
+        return f'<User {self.username}>'
 
-def to_dict(self):
-    return {
-        "id": self.id, 
-        "username": self.username,
-        "role": self.role,
-    }
+    def to_dict(self):
+        return {
+            "id": self.id, 
+            "username": self.username,
+            "role": self.role,
+        }
+#fixed indentation, functions outside class :>
