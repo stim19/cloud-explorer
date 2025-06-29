4ec7a8fa78a96d5cc7229e29eb800575d0b34f29
stim19 <yoshiyomimiyori@gmail.com>
2025-06-28 20:00:08 -0400

    updated project stuff!

--- utils/auth.py
+++ utils/auth.py
@@ -1,16 +1,33 @@
 from flask_login import UserMixin
-USERS = {
-	"a": {"password":"a"}
-}
+from werkzeug.security import generate_password_hash,check_password_hash
+from extensions import db #make sure this points to your actual db instance
 
-class User(UserMixin):
 
-	def __init__(self, username):
-		self.id = username
 
-	@staticmethod
-	def validate(username, password):
-		user = USERS.get(username)
-		if user and user['password']==password:
-			return User(username)
-		return None
+class User(db.Model, UserMixin):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(64), unique=True, nullable=False)
+    password_hash=db.Column(db.String(128), nullable=False)
+    role=db.Column(db.String(10), default='user', nullable=False)
+
+#Write only
+@property
+def password(self):
+    raise AttributeError("Password is write-only")
+
+@password.setter
+def password(self, password):
+    self.password_hash=generate_password_hash(password)
+
+def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+def __repr__(self):
+    return f'<User {self.username}>'
+
+def to_dict(self):
+    return {
+        "id": self.id, 
+        "username": self.username,
+        "role": self.role,
+    }
